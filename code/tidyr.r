3d63f943a6b697afeee0007a913dbdf56f85f222
Jared Lander <info@jaredlander.com>
2018-03-08 12:51:56 -0500

    Started tidyr

---
+++ code/tidyr.r
@@ -0,0 +1,15 @@
+library(readr)
+
+emotion <- read_tsv('data/reaction.txt')
+
+head(emotion)
+
+library(tidyr)
+
+emotion %>% 
+    gather(key=Type, value=Measurement, Age, BMI, React, Regulate)
+
+emotionLong <- emotion %>% 
+    gather(key=Type, value=Measurement, Age, BMI, React, Regulate)
+
+View(emotionLong)
