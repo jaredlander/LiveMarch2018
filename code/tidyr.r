1d9f456faebb6242c77e3b8c2f69fd6b528d4ef8
Jared Lander <info@jaredlander.com>
2018-03-08 13:02:50 -0500

    finished tidyr

--- code/tidyr.r
+++ code/tidyr.r
@@ -13,3 +13,13 @@ emotionLong <- emotion %>%
     gather(key=Type, value=Measurement, Age, BMI, React, Regulate)
 
 View(emotionLong)
+
+emotionLong %>% 
+    spread(key=Type, value=Measurement)
+
+emotionLong %>% 
+    spread(key=Type, value=Measurement) %>% 
+    dplyr::arrange(ID, Test)
+
+separate
+unite
