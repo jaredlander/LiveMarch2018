8d05c8636980430d50b93e4484fd8da9888e9d97
Jared Lander <info@jaredlander.com>
2018-03-09 12:10:18 -0500

    Added reading html tables

--- code/ReadHTML.r
+++ code/ReadHTML.r
@@ -15,3 +15,13 @@ ribalta %>% html_nodes('.street') %>% html_text()
 
 ribalta %>% html_nodes('#latitude')
 ribalta %>% html_nodes('#latitude') %>% html_attr('value')
+
+ribalta %>% html_nodes('table.food-items')
+ribalta %>% 
+    html_nodes('table.food-items') %>% 
+    magrittr::extract2(5)
+
+ribalta %>% 
+    html_nodes('table.food-items') %>% 
+    magrittr::extract2(5) %>% 
+    html_table()
